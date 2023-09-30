b57c4efee67b5149011a27ff04d9d316a6dfb651
Maksim Gusev <51376126+AlanTheKnight@users.noreply.github.com>
2023-09-30 11:24:45 +0300

    Improve lab #1 p.5

--- lab-1/solution/solution.sh
+++ lab-1/solution/solution.sh
@@ -131,16 +131,20 @@ cat -n ~/lab0/*/c* ~/lab0/c* 2>/tmp/error.log | sort -r -k 2,2
 rm ~/lab0/beheeyem4
 
 # Удалить файл lab0/buneary0/finneon:
+chmod u+w ~/lab0/buneary0
 rm ~/lab0/buneary0/finneon
+chmod u-w ~/lab0/buneary0
 
 # Удалить символические ссылки Copy_*:
 rm ~/lab0/Copy_*
 
 # Удалить жесткие ссылки lab0/tirtouga1/pidgeottoswal*:
+chmod u+w ~/lab0/tirtouga1
 rm ~/lab0/tirtouga1/pidgeottoswal*
+chmod u-w ~/lab0/tirtouga1
 
 # Удалить директорию buneary0:
 rm -r ~/lab0/buneary0
 
 # Удалить директорию lab0/totodile2/jumpluff:
-rm -r ~/lab0/totodile2/jumpluff
+rmdir ~/lab0/totodile2/jumpluff
