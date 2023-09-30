d29ee40a2cf1b7c416c9c7a4155ec3600f064d79
Maksim Gusev <51376126+AlanTheKnight@users.noreply.github.com>
2023-09-30 11:41:17 +0300

    Add force option to commands in lab #1 p.5

--- lab-1/solution/lab-1-5.sh
+++ lab-1/solution/lab-1-5.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/bash
 
 # Удалить файл beheeyem4:
-rm ~/lab0/beheeyem4
+rm -f ~/lab0/beheeyem4
 
 # Удалить файл lab0/buneary0/finneon:
 chmod u+w ~/lab0/buneary0
-rm ~/lab0/buneary0/finneon
+rm -f ~/lab0/buneary0/finneon
 chmod u-w ~/lab0/buneary0
 
 # Удалить символические ссылки Copy_*:
-rm ~/lab0/Copy_*
+rm -f ~/lab0/Copy_*
 
 # Удалить жесткие ссылки lab0/tirtouga1/pidgeottoswal*:
 chmod u+w ~/lab0/tirtouga1
-rm ~/lab0/tirtouga1/pidgeottoswal*
+rm -f ~/lab0/tirtouga1/pidgeottoswal*
 chmod u-w ~/lab0/tirtouga1
 
 # Удалить директорию buneary0:
-rm -r ~/lab0/buneary0
+chmod u+w ~/lab0/buneary0
+rm -rf ~/lab0/buneary0
 
 # Удалить директорию lab0/totodile2/jumpluff:
 rmdir ~/lab0/totodile2/jumpluff
