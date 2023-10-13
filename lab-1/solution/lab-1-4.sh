9115e85a3c573684662cd45b4b92630ffed46ca6
Maksim Gusev <51376126+AlanTheKnight@users.noreply.github.com>
2023-10-13 23:30:15 +0300

    Some changes

--- lab-1/solution/lab-1-4.sh
+++ lab-1/solution/lab-1-4.sh
@@ -4,32 +4,35 @@
 # swablu, glalie, finneon, pidgeotto, camerupt
 # отсортировать вывод по увеличению количества,
 # ошибки доступа не подавлять и не перенаправлять
-wc -m ~/lab0/buneary0/swablu ~/lab0/buneary0/glalie ~/lab0/buneary0/finneon ~/lab0/tirtouga1/pidgeotto ~/lab0/tirtouga1/chimchar | sort -n
+
+# wc -m ~/lab0/buneary0/swablu ~/lab0/buneary0/glalie ~/lab0/buneary0/finneon ~/lab0/tirtouga1/pidgeotto ~/lab0/tirtouga1/chimchar | sort -n
 
 # Вывести список имен и атрибутов файлов в директории
 # totodile2, список отсортировать по возрастанию размера,
 # ошибки доступа не подавлять и не перенаправлять
-ls -lgoSr ~/lab0/totodile2 | grep -v '^total'
+
+# ls -lSr ~/lab0/totodile2 | grep -v '^total'
+# ls -l ~/lab0/totodile2 | grep -v '^total' | sort -rk5,5n
 
 # Рекурсивно вывести содержимое файлов из директории
 # lab0, имя которых заканчивается на 't', строки
 # отсортировать по имени a->z, ошибки доступа перенаправить
 # в файл в директории /tmp
-cat ~/lab0/*/*t | sort 2>/tmp/s408512-error.log
+# cat ~/lab0/*/*t ~/lab0/*t | sort 2>/tmp/s408512-error.log
 
 # Вывести список имен и атрибутов файлов в директории buneary0,
 # список отсортировать по убыванию размера, ошибки доступа
 # перенаправить в файл в директории /tmp
-ls -loSg ~/lab0/buneary0 2>/tmp/s408512-error.log
+# ls -loSg ~/lab0/buneary0 2>/tmp/s408512-error.log
 
 # Вывести четыре последних элемента рекурсивного списка имен
 # и атрибутов файлов в директории lab0, начинающихся на символ
 # 'c', список отсортировать по возрастанию количества жестких
 # ссылок, ошибки доступа перенаправить в файл в директории /tmp
-ls -Rlgo ~/lab0 2>/tmp/s408512-error.log | grep -v "^total" | grep " " | grep " c" | sort -rnk 2,2 | tail -n 4
+# ls -Rl ~/lab0 2>/tmp/s408512-error.log | grep " c" | sort -rnk 2,2 | tail -n 4
 
 # Рекурсивно вывести содержимое файлов с номерами строк из
 # директории lab0, имя которых начинается на 'c', строки
 # отсортировать по имени z->a, ошибки доступа перенаправить
 # в файл в директории /tmp
-cat -n ~/lab0/*/c* ~/lab0/c* 2>/tmp/s408512-error.log | sort -r -k 2,2
+cat -n ~/lab0/c* ~/lab0/*/c* ~/lab0/c* 2>/tmp/s408512-error.log | sort -r -k 2,2
